75aa30a1119fd521839fd2d2fb70eac829071bbb
Lanka Rathanyaka <rmllbrathnayaka@gmail.com>
2020-11-24 13:27:31 +0000

    Adding the Home Golder

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const _Home=()=>{
+  return(
+    <div className="jumbotron"> 
+    <h1>Dashboard</h1>
+    <p>Find all your books here</p>
+    
+    </div>
+  )
+}
+
+export const Home=_Home
